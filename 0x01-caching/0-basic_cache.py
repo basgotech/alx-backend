ab84577aa33fc4248ca6876ca70abbc4d155a2bf
Basliel Tegegn <basgolilo5@gmail.com>
2024-07-24 02:57:59 -0700

    add

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -16,10 +16,6 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Adds the item to the cache with the key.
             If either key or item is None, nothing is added.
-        
-            Args:
-                key (str): The key under which the item is stored.
-                item (any): The item to store in the cache.
         """
         
         if key is not None and item is not None:
@@ -28,12 +24,6 @@ class BasicCache(BaseCaching):
     def get(self, key):
         """ Retrieves the item from the cache with the specified key.
             If the key is None or doesn't exist, returns None.
-        
-            Args:
-                key (str): The key of the item to retrieve.
-        
-            Returns:
-                any: The item stored in the cache, or None if the key is not found.
         """
         
         return self.cache_data.get(key, None)
