ca675f28945ce2876423a402d6df9552472d567c
Basliel Tegegn <basgolilo5@gmail.com>
2024-07-24 02:59:25 -0700

    fix

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,20 +9,24 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ BasicCache class that inherits from BaseCaching and implements
-        basic dictionary storage with put and get methods.
+    """ BasicCache class that inherits
+        from BaseCaching and implements
+        basic dictionary storage with put
+        and get methods.
     """
 
     def put(self, key, item):
         """ Adds the item to the cache with the key.
-            If either key or item is None, nothing is added.
+            If either key or item is None,
+            nothing is added.
         """
         
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Retrieves the item from the cache with the specified key.
+        """ Retrieves the item from the cache
+        with the specified key.
             If the key is None or doesn't exist, returns None.
         """
         
