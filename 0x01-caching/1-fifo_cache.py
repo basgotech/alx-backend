b0227e857dba9aa1c5cf97025aad9492dbd34737
Basliel Tegegn <basgolilo5@gmail.com>
2024-07-24 03:08:56 -0700

    fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -9,20 +9,17 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    '''FIFOCache class that inherits from BaseCaching and implements
-        a FIFO (First In, First Out) caching system.
+    '''A class `FIFOCache` that inherits from
+       `BaseCaching` and is a caching system.
     '''
 
     def __init__(self):
-        '''
-         Initialize the cache with an ordered dictionary to maintain
-            the order of insertion.
-        '''
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        '''Adds the item to the cache with the specified key.
+        '''assign to the dictionary `self.cache_data` the
+           `item` value for the key `key`
         '''
 
         if key is None or item is None:
@@ -35,6 +32,6 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        ''' Retrieves the item from the cache with the specified key.
+        '''return the value in `self.cache_data` linked to `key`
         '''
         return self.cache_data.get(key, None)
